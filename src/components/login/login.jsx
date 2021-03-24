4846a0d58efb7ac3b68bba29f408c7ed63aa630a
carlosvicient <carlos.vicient@ntnu.no>
2021-03-24 11:01:52 +0100

    login with provider

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import { AuthContext } from '../../helpers/Auth';
+import { Redirect } from "react-router-dom";
 
 class Login extends Component {
     static contextType = AuthContext;
-    
+    state = { redirect: false };
+
     componentDidMount() {
         const auth = this.context;
         console.log(auth) // { name: 'Tania', loggedIn: true }
     }
 
+    handleLogOut = () => {
+        this.context.logout();
+    }
+
+    handleLogIn = () => {
+        this.context.login();
+        this.setState({redirect: '/user'});
+    }
+
     render() {
+        if (this.state.redirect)
+            return (<Redirect to={this.state.redirect} />);
+
         return (
             <div className="Login">
-                {this.context.isAuth && <button onClick={this.context.logout}>Log out</button>}
-                {!this.context.isAuth && <button onClick={this.context.login}>Log in</button>}
+                {this.context.isAuth && <button onClick={this.handleLogOut}>Log out</button>}
+                {!this.context.isAuth && <button onClick={this.handleLogIn}>Log in</button>}
             </div>
         );
     }
